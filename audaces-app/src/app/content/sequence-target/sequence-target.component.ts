e148d7e85956eed72bc1b664f83f89ae142a20e5
gabardolucas <lucasgabar@gmail.com>
2021-02-01 20:42:35 -0300

    teste 3

--- audaces-app/src/app/content/sequence-target/sequence-target.component.ts
+++ audaces-app/src/app/content/sequence-target/sequence-target.component.ts
@@ -11,58 +11,58 @@ export class SequenceTargetComponent {
   aditionalFields: any[] = [];
   target: any = null;
   combinations: any = null;
-  
 
-  addField(){
+
+  addField() {
     this.aditionalFields.push(null);
   }
 
-  clearFields(){
+  clearFields() {
     this.sequence = [null, null, null, null];
     this.aditionalFields = [];
     this.combinations = null
-    this.target = null; 
+    this.target = null;
   }
 
-  subsetSum(sequence: any[], target: number, r=[], s=target){    
+  subsetSum(sequence: any[], target: number, r = [], s = target) {
     if (s == 0)
       return [r];
-    
-    
+
+
     let result: any[] = [];
-    
-    for (let a of sequence){
-      if (a <= s){
-        if(result.length < 10){
+
+    for (let a of sequence) {
+      if (a <= s) {
+        if (result.length < 10) {
           result = result.concat(
-            this.subsetSum(sequence, target, r.slice().concat(a), s-a));
-        }     
+            this.subsetSum(sequence, target, r.slice().concat(a), s - a));
+        }
       }
-    }      
-      
+    }
+
     return result;
-  }  
-
-  combine(){
-    this.combinations = []
-
-    let sequenceDesc = this.sequence.concat(this.aditionalFields).map((item) => {      
-      return parseInt(item);      
-    })
-    .filter(s => typeof s !== "undefined" && s !== 0 && !isNaN(s))
-    .sort((a: any, b: any) => {return b - a});
-
-    let result = this.subsetSum(sequenceDesc, parseInt(this.target));        
-    this.combinations = result ? this.getSumArray(result) : [];
   }
 
-  getSumArray(result: any[]){
-    let sumArray : any[] = [];
+  combine() {
+    this.combinations = []
+
+    let sequenceDesc = this.sequence.concat(this.aditionalFields).map((item) => {
+      return parseInt(item);
+    })
+      .filter(s => typeof s !== "undefined" && s !== 0 && !isNaN(s))
+      .sort((a: any, b: any) => { return b - a });
+
+    let result = this.subsetSum(sequenceDesc, parseInt(this.target));
+    this.combinations = result ? this.getSumArray(result) : [];
+  }
+  // teste 3
+  getSumArray(result: any[]) {
+    let sumArray: any[] = [];
 
     result.forEach((arrayNumbers) => {
       let sumItem = '';
 
-      arrayNumbers.forEach((item:number) => {
+      arrayNumbers.forEach((item: number) => {
         sumItem += `${item} + `;
       })
 
